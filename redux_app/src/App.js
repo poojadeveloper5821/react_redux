6311517cfac572a9ae7e8b87363df7d7ce21edf7
pdongre123 <pdongre040@gmail.com>
2022-05-12 16:16:54 +0530

    https://github.com/poojadeveloper5821/React_Redux_Concepts

---
+++ redux_app/src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {actions} from './store/index';
+
+function App() {
+  const counter=useSelector((state)=>state.counter);
+  const dispatch=useDispatch()
+  const increment=()=>{
+    dispatch(actions.increment());
+  };
+  const decrement=()=>{
+    dispatch(actions.decrement());
+  };
+  const addBy=()=>{
+    dispatch(actions.addBy(10));
+  }
+
+  return (
+    <div> 
+      <h1>Counter App</h1>
+      <h2>{counter}</h2>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={addBy}>Add By 10</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ redux_app/src/store/index.jsx
@@ -0,0 +1,86 @@
+import {configureStore,createSlice} from "@reduxjs/toolkit";
+
+const counterSlice= createSlice({
+    name:'counter',
+    initialState : {counter:0},
+    reducers:{
+        increment(state,action){
+            state.counter++;
+        },
+        decrement(state,action){
+            state.counter--;
+        },
+        addBy(state,action){
+            state.counter += action.payload;
+        }
+    }
+})
+
+export const actions=counterSlice.actions;
+const store= configureStore({
+    reducer:counterSlice.reducer
+})
+
+export default store;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { createStore } from "redux";
+
+// const reducerFn = (state = { counter: 0 }, action) => {
+//   //synchronus function
+//   //we should not mutate the original state
+//   if (action.type === "INC") {
+//     return { counter: state.counter + 1 };
+//   }
+//   if (action.type === "DEC") {
+//     return { counter: state.counter - 1 };
+//   }
+//   if (action.type === "ADD") {
+//     return { counter: state.counter + action.payload };
+//   }
+
+//   return state;
+// };
+// const store = createStore(reducerFn);
+// export default store;
