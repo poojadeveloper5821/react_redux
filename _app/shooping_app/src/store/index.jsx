74ed330b917be7c87529ca22858b8d79489ff78a
poojadeveloper5821 <100752964+poojadeveloper5821@users.noreply.github.com>
2022-05-17 14:34:51 +0530

    Delete _app directory

--- _app/shooping_app/src/store/index.jsx
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import authSlice from "./auth_slice";
-
-const store = configureStore({
-  reducer: {
-    auth: authSlice.reducer,
-  },
-});
-export default store;
